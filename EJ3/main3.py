ad2a0032f50e16eb710f9bc85715625f0a98b339
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-28 17:27:25 +0200

    main acabado y decorado antes de empezar cn lo bno

--- EJ3/main3.py
+++ EJ3/main3.py
@@ -3,8 +3,15 @@ from clasesej3 import (Nave,HalconMilenario,EstrellaDeLaMuerte)
 def main3():
     ds=EstrellaDeLaMuerte()
     hm=HalconMilenario()
-    
+    print('================================')
+    print('HACLON MILENARIO CARACTERISTICAS')
+    print('================================\n')
+    hm.__str__()
+    print('================================')
+    print('ESTRELLA DE LA MUERTE CARACTERIS.')
+    print('================================\n')
     ds.__str__()
+    print('================================')
 
 
 if __name__=='__main__':
