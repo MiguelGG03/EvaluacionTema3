16dda08f14eab89061a35f6d6392aad7e5c25d81
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-28 16:13:48 +0200

    cosas de agregar eliminadas, bernstein eliminado

--- EJ5/tablas_hash.py
+++ EJ5/tablas_hash.py
@@ -11,18 +11,10 @@ class Hash(object):
         self.tabla=[]
         self.tamanio=0
 
-    def bernstein(self,cadena):
-        '''Solo Funciona para cadenas de caracteres'''
-        h=0
-        for caracter in cadena:
-            h = h *33 + ord(caracter)
-        return h
-
     def encriptador(self,dato):
         '''Encripta el texto'''
         resultado=int(hashlib.sha256(dato.encode('utf-8')).hexdigest(), 16) % 10**8
-        return resultado
-    
+        return resultado  
 
     def crear_tabla(self,tamanio):
         self.tabla=[None]*tamanio
@@ -41,12 +33,4 @@ class Hash(object):
         if(tabla[posicion] is None):
             tabla[posicion]=dato
             print(f'{dato} instertado con exito a la tabla en la posicion {posicion}')
-            self.tabla=tabla
-        else:
-            aux2=tabla[posicion]
-            while(aux2.sig!=None):
-                aux2=aux2.sig            
-            aux2.sig=aux
-            print(f'Se ha adherido ({aux2.info}) a la cola con exito, su puntero apunta a ({aux2.sig.info}),\nel cual apunta a (None)')
-            print(f'Ahora el nodo ({aux2.info}) apunta hacia ({aux2.sig.info})')
             self.tabla=tabla
