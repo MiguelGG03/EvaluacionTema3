4e946a73718ab89886a07049807adf9a641d3194
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-29 12:12:34 +0200

    intentando hacer el top 5 tripulacion

--- EJ3/main3.py
+++ EJ3/main3.py
@@ -1,5 +1,21 @@
 from clasesej3 import (HalconMilenario,EstrellaDeLaMuerte)
 
+def listador(algo):
+    lista=[]
+    hm=HalconMilenario()
+    ds=EstrellaDeLaMuerte()
+    lista.append(ds.nave1)
+    lista.append(ds.nave2)
+    lista.append(ds.nave3)
+    lista.append(ds.nave4)
+    lista.append(ds.nave5)
+    cont=0
+    for i in range(5):
+        if(lista[i].tripulacion<cont):
+            cont=lista[i].tripulacion
+
+        
+
 def main3():
     ds=EstrellaDeLaMuerte()
     hm=HalconMilenario()
@@ -12,6 +28,10 @@ def main3():
     print('================================\n')
     ds.__str__()
     print('================================')
+    print('NAVES MAYOR NUMERO DE TRIPULACION')
+
+
+
 
 
 if __name__=='__main__':
