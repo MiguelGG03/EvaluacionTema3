4b16022068783019676fba967f56f0598fa216ec
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-31 09:17:51 +0100

    no importa el modulo clasesej3

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from EJ5.main5 import main5
 from EJ4.main4 import main4
 
 def main():
-    pr1=input('Que ejercicio deseas ver (1,2,3==1)(4)(5): ')
+    pr1=input('Que ejercicio deseas ver (1,2,3,4,5): ')
 
     if pr1=='1':
         main1()
     elif pr1=='2':
-        main4()
+        main2()
     elif pr1=='3':
-        main4()
+        main3()
     elif pr1=='4':
         main4()
     elif pr1=='5':
