889a56f2239f4ca6a1ce91cc27091b8a2db0c065
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-11-23 16:36:38 +0100

    no va uwu

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,6 @@
 import sys
 
 print(sys.path)
-sys.path.append('C:\\Users\\mglez\\OneDrive\\Escritorio\\GitHub\\EvaluacionTema3\\EJ3\\clasesej3.py')
+sys.path.instert('C:\\Users\\mglez\\OneDrive\\Escritorio\\GitHub\\EvaluacionTema3\\EJ3\\clasesej3.py')
 sys.path.append('C:\\Users\\mglez\\OneDrive\\Escritorio\\GitHub\\EvaluacionTema3\\EJ3\\main3.py')
 print(sys.path)
